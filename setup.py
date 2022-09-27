eae39c144fe4dd8ae3fe68d5562d54318a26e9b1
anacmolina <anacmolina@udea.edu.co>
2022-09-27 01:29:22 -0500

    test not working yet, see if simple test works

--- setup.py
+++ setup.py
@@ -65,8 +65,7 @@ def main():
         # See: https://github.com/pypa/pipenv/issues/2171
         install_requires=['numpy==1.16.5; pandas==1.3.5; python_version=="3.7"',
             'numpy>=1.16.5; pandas>=1.3.5, python_version=="3.8"',
-            'numpy>=1.16.5; pandas>=1.3.5, python_version=="3.9"',
-            'numpy'],
+            'numpy>=1.16.5; pandas>=1.3.5, python_version=="3.9"'],
     )
 
 

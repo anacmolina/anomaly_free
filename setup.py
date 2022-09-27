63607e87e3511171142d9239de8f784323f0743b
anacmolina <anacmolina@udea.edu.co>
2022-09-27 01:27:12 -0500

    test not working yet, see if simple test works

--- setup.py
+++ setup.py
@@ -66,8 +66,7 @@ def main():
         install_requires=['numpy==1.16.5; pandas==1.3.5; python_version=="3.7"',
             'numpy>=1.16.5; pandas>=1.3.5, python_version=="3.8"',
             'numpy>=1.16.5; pandas>=1.3.5, python_version=="3.9"',
-            'numpy',
-            'pandas'],
+            'numpy'],
     )
 
 

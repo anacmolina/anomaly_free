e3dc163a09792a750d755f9feb68cdfa7b3642ef
anacmolina <anacmolina@udea.edu.co>
2022-09-27 11:03:25 -0500

    pytest locally working

--- tests/test_anomaly_free.py
+++ tests/test_anomaly_free.py
@@ -12,30 +12,10 @@ inputs = {
     "output_name": "solution",
 }
 
-sls_5 = pd.DataFrame(columns=["z"])
-data_5 = [
-    [10, -9, -7, 4, 2],
-    [26, -25, -14, 9, 4],
-    [28, -25, -23, 18, 2],
-    [20, -18, -17, 14, 1],
-    [26, -22, -20, 9, 7],
-    [22, -21, -12, 6, 5],
-    [9, -8, -7, 5, 1],
-    [27, -25, -17, 8, 7],
-    [27, -26, -14, 8, 5],
-    [28, -26, -18, 11, 5],
-    [25, -22, -18, 8, 7],
-]
-
-sls_5.z = data_5
-
-print(sls_5)
-
-
 class Test_anomaly(unittest.TestCase):
     def test_working(self):
-        sls = methods.find_several_set(**inputs)
-        self.assertEqual(sls['z'].to_numpy().all(), sls_5.to_numpy().all(), True)
+        sls = methods.find_several_set(**inputs).z.to_numpy()
+        self.assertEqual(11, sls.shape[0], True)
 
 
 if __name__ == "__main__":
